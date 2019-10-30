031793683be25d54dc99a93508d927631ab8c921
CHUNGSIK PARK <chungsikpark@CHUNGSIKui-MacBookPro.local>
2019-10-30 17:52:35 +0900

    EventLists component add

---
+++ src/components/EventLists/EventsLists.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './EventsLists.scss'
+
+export class EventsLists extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default EventsLists
