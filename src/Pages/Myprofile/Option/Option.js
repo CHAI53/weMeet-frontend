bd34c13178b8f4dfc984c5107e5f0d708eb11cec
kyj7337 <kyj7337.github.io>
2019-11-04 15:38:36 +0900

    지금도 대문자 인가요

---
+++ src/Pages/Myprofile/Option/Option.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import './Option.scss'
+
+class Option extends Component{
+    render(){
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default Option;
