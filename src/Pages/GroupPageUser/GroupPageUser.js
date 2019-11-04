779922c1d4bbfc8433873fd8279fb9c498ac9c0f
CHUNGSIK PARK <chungsikpark@CHUNGSIKui-MacBookPro.local>
2019-11-04 22:32:59 +0900

    group page user initial commit

---
+++ src/Pages/GroupPageUser/GroupPageUser.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './GroupPageUser.scss';
+
+export class GroupPageUser extends Component {
+    render() {
+        return (
+            <>
+            </>
+        )
+    }
+}
+
+export default GroupPageUser
--- src/Routes.js
+++ src/Routes.js
@@ -6,6 +6,7 @@ import Signup from "Pages/Signup";
 import Event from "./Pages/Event";
 import Login from "./Pages/Login";
 import Myprofile from "./Pages/Myprofile"
+import GroupPageUser from "./Pages/GroupPageUser";
 
 class Routes extends React.Component {
   render() {
@@ -18,7 +19,7 @@ class Routes extends React.Component {
           <Route exact path="/myprofile" component={Myprofile}/>
           <Route exact path="/after_login_main" component={AfterLoginMain} />
           <Route exact path="/event/:id" component={Event} />
-
+          <Route exact path="/group_page_user" component={GroupPageUser} />
         </Switch>
       </Router>
     );
