02b975ee4fe10bbb9fe79c67541fa454a2f605fa
CHUNGSIK PARK <chungsikpark@CHUNGSIKui-MacBookPro.local>
2019-11-06 22:08:25 +0900

    group user page done without api

--- src/components/GroupPageUser/UpcomingEvents/SingleUpcomingEvent/SingleUpcoming.js
+++ src/components/GroupPageUser/UpcomingEvents/SingleUpcomingEvent/SingleUpcoming.js
@@ -3,20 +3,31 @@ import './SingleUpcoming.scss'
 
 export class SingleUpcoming extends Component {
     render() {
+        const SingleEventsLabelImg = (src) => ({
+            backgroundImage: `url(${src})`
+        })
+
         return (
             <div className="single-upcoming">
                 <div className="single-upcoming-in">
                     <div className="single-upcoming-brief">
                         <div className="single-upcoming-brief-in">
                             <div className="single-upcoming-brief-left">
-                                <div className="single-upcoming-date">11월 9일 (토), 오후 6:50</div>
-                                <div className="single-upcoming-name">NEW PLACE FREE SATURDAY INTERNATIONAL FRIENDS MAKING BEER PARTY+CLUBBING!</div>
-                                <div className="single-upcoming-place"><span className="place-icon"></span><span>MCJUCHANGO/맥주창고</span></div>
+                                <div className="single-upcoming-date">
+                                    {this.props.date}
+                                </div>
+                                <div className="single-upcoming-name">
+                                    {this.props.name}
+                                </div>
+                                <div className="single-upcoming-place">
+                                    <span className="place-icon"/>
+                                    <span>{this.props.place}</span>
+                                </div>
                             </div>
                             <div className="single-upcoming-brief-right">
                                 <span
                                     className="single-upcoming-img"
-                                    style={{ backgroundImage: `url(https://secure.meetupstatic.com/photos/event/3/9/3/5/600_486374645.jpeg)`}}
+                                    style={SingleEventsLabelImg(this.props.imageurl)}
                                 >
                                 </span>
                             </div>
@@ -24,59 +35,7 @@ export class SingleUpcoming extends Component {
                     </div>
                     <div className="single-upcoming-text">
                         <p className="single-upcoming-text-in">
-                        No Entrance Fee Party - 오셔서 마음껏 파티를 즐기세요~ Enjoy the party without any entrance fee.
-                        Free Entrance Fee (무료입장) - 단 맥주는 구매하셔야 합니다. But you have to buy beer.
-
-                        2차는 강남 클럽에 무료입장 가능합니다!
-                        2nd round we will go to a Gangnam club.
-
-                        영어 못하시더라도 부담없이 친구 많이 데려 오세요!
-                        
-                        장소 주소(Place address): 2nd Floor Mcjuchango
-                        646-4 Yeoksam 1(il)-dong, Gangnam-gu, Seoul
-                        (강남구 역삼1동 646-4, 2층 맥주창고)
-                        
-                        Location: Yeoksam Station(2nd line, green line) exit no,4 MCJUCHANGGO/이층 맥주창고 (4mins away from subway station by walk)
-                        Please come to MCJUCHANGGO/이층 맥주창고. MCJUCHANGGO is for us!
-                        Get out of exit no. 4 of Yeoksam Station and go 1block and then you can see Yankee Candle store. Turn right towards the alley at the Yankee Candle store and keep going straight until you can see Occoa and the Whiskey. In front of The Whiskey you will see the"PAYOT" Sign Building. Come to the 2nd floor of "PAYOT" Sign Building. On the 2nd floor you will see Stella beer and Asahi beer sign. We have a party there. We have a party there.
-                        
-                        장소: 역삼역 4번출구 맥주창고 (역삼역에서 걸어서 4분거리)
-                        역삼역 4번출구에서 나오셔서 양키캔들이 보일때까지 직진하세요 양키캔들을 끼고 우회전하신 후 명인만두가 보일때까지 한블럭만 직진하시고 명인만두가 보이시면 좌회전하시면 2층에 바로 이층 맥주창고가 보입니다. 2층으로 오시면 됩니다.
-                        
-                        Entrance fee : Free (무료) 단 맥주는 구매하셔야 합니다. But you have to buy beer.
-                        
-                        TIME: 6:45PM ~ 1:00PM (5HOURS) After the Party we will go to the Clubs in Gangnam for free. It is close to SEULUNG station and SINSA station and Chungdam station! 파티후 강남의 핫한 클럽을 무려로 가보아요!
-                        
-                        Over 60 people are coming to join us every Saturday and having fun talking with people in English. 매주 60이상이 넘게 참여하여 영어로 즐기고 있어요!
-                        
-                        DIRECTION PICTURE BELOW! 아래 오시는법 사진을 참조하세요!
-                        
-                        맥주는 따로 구매하셔야 한답니다
-                        Beer is not include in the entrance fee. Please buy your own beer.
-                        
-                        See our party video below
-                        
-                        https://www.youtube.com/watch?v=Xp5e_CxKM3k
-                        
-                        https://www.youtube.com/watch?v=u-8Yge-E-AY
-                        
-                        https://www.youtube.com/watch?v=8e4LlQTaenk
-                        
-                        60~70 people come to join us and drink and party together! Ratio of the girls are very high!
-                        
-                        보통 60~70분 정도 참석하셔서 드링킹과 파티를 즐기고 있어요!여성분들의 참여도도 가장 높은 모임이에요!
-                        
-                        한국어, 영어 모든 언어 사용가능합니다. 영어 못하시더라도 부담없이 친구 많이 데려 오세요!
-                        
-                        Over 60 people are coming to join us every Saturday and having fun talking with people in English.
-                        매주 60분 이상 참석하셔서 즐거운 시간을 보내고 있습니다
-                        
-                        North & South Americans, Europeans, Asians, Africans and so on anyone can join our SPECIAL SATURDAY PARTY!
-                        북미, 남미, 유럽, 아시안, 아프리칸! 어디서온 누구든지 인터네셔널 SPECIAL SATURDAY PARTY!에 참석할 수 있어요!
-                        
-                        Gentlemen & Ladies meet internationally with drinking international beer!
-                        
-                        남자분 & 여자분이 맥주를 마시며 인터네셔널하게 만나 볼까요?
+                            {this.props.text}
                         </p>
                     </div>
                     <div className="single-upcoming-attend">
