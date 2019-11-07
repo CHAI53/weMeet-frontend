48a37824fb83906f6fcb331f8918a7efff40cabc
CHUNGSIK PARK <chungsikpark@CHUNGSIKui-MacBookPro.local>
2019-11-07 16:12:16 +0900

    initial commit

---
+++ src/Pages/MasterGroupPage/MasterGroupPage.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './MasterGroupPage.scss'
+
+export class MasterGroupPage extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default MasterGroupPage
