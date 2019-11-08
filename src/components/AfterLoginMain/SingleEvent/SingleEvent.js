5d422db464242d238b35e445d19d92df094737ec
CHUNGSIK PARK <chungsikpark@CHUNGSIKui-MacBookPro.local>
2019-11-08 11:44:08 +0900

    event page route setting

--- src/components/AfterLoginMain/SingleEvent/SingleEvent.js
+++ src/components/AfterLoginMain/SingleEvent/SingleEvent.js
@@ -1,11 +1,18 @@
 import React from "react";
 import "./SingleEvent.scss";
+import { withRouter } from "react-router-dom";
 
-export default function SingleEvent(props) {
+function SingleEvent(props) {
+  const handleclick = () => {
+    const id = props.eventId;
+    props.history.push(`/event/${id}`);
+    console.log(id);
+    console.log(props);
+  };
   const { groupName, eventName, expectMembers } = props;
   return (
     <li className="single-event-list">
-      <div className="single-event-content">
+      <div className="single-event-content" onClick={handleclick}>
         <div className="single-event-group">{groupName}</div>
         <div className="single-event-name">{eventName}</div>
         <div className="expect-members">{expectMembers}</div>
@@ -13,3 +20,5 @@ export default function SingleEvent(props) {
     </li>
   );
 }
+
+export default withRouter(SingleEvent);
